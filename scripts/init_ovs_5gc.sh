13de85f9d996c3d9d7b6c6baf6ce2d535a67e971
Johnson <johnsonli1993@gmail.com>
2022-10-07 10:54:51 +0300

    update scripts

--- scripts/init_ovs_5gc.sh
+++ scripts/init_ovs_5gc.sh
@@ -2,6 +2,6 @@ sudo ovs-vsctl del-br br0
 sudo ovs-vsctl add-br br0
 sudo ovs-vsctl set bridge br0 other_config:hwaddr=3a:4d:a7:05:2a:45
 sudo ovs-vsctl add-port br0 gre0 -- set interface gre0 type=gre options:remote_ip=192.168.56.102
-sudo ifconfig br0 10.0.2.15
+sudo ifconfig br0 10.0.10.15
 sudo ip route del default
-sudo ip route add default via 10.0.2.14 dev br0
+sudo ip route add default via 10.0.10.14 dev br0
--- scripts/init_ovs_vm3.sh
+++ scripts/init_ovs_vm3.sh
@@ -1,4 +1,4 @@
 sudo ovs-vsctl del-br br0
 sudo ovs-vsctl add-br br0
 sudo ovs-vsctl add-port br0 gre0 -- set interface gre0 type=gre options:remote_ip=192.168.56.102
-sudo ifconfig br0 10.0.2.13
+sudo ifconfig br0 10.0.10.13
