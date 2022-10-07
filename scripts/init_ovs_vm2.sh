dd3972dd150b4669cece95767aaf0243cbbd3a70
Johnson <johnsonli1993@gmail.com>
2022-10-07 10:41:27 +0300

    add gre between vm2 and vm3

--- scripts/init_ovs_vm2.sh
+++ scripts/init_ovs_vm2.sh
@@ -1,6 +1,7 @@
 sudo ovs-vsctl del-br br0
 sudo ovs-vsctl add-br br0
 sudo ovs-vsctl add-port br0 gre0 -- set interface gre0 type=gre options:remote_ip=192.168.56.104
+sudo ovs-vsctl add-port br0 gre1 -- set interface gre0 type=gre options:remote_ip=192.168.56.103
 sudo ifconfig br0 10.0.2.14
 sudo modprobe iptable_nat
 sudo sysctl -w net.ipv4.ip_forward=1
