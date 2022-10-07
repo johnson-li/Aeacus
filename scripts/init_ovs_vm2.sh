aba83b716fe74fe1b8bb2dd2432659fca88c575c
Johnson <johnsonli1993@gmail.com>
2022-10-07 10:54:12 +0300

    update scripts

--- scripts/init_ovs_vm2.sh
+++ scripts/init_ovs_vm2.sh
@@ -1,8 +1,8 @@
 sudo ovs-vsctl del-br br0
 sudo ovs-vsctl add-br br0
 sudo ovs-vsctl add-port br0 gre0 -- set interface gre0 type=gre options:remote_ip=192.168.56.104
-sudo ovs-vsctl add-port br0 gre1 -- set interface gre0 type=gre options:remote_ip=192.168.56.103
-sudo ifconfig br0 10.0.2.14
+sudo ovs-vsctl add-port br0 gre1 -- set interface gre1 type=gre options:remote_ip=192.168.56.103
+sudo ifconfig br0 10.0.10.14
 sudo modprobe iptable_nat
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
