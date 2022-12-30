2b7d08dfda8d9e70172dce28a109becd07cf5166
Johnson <johnsonli1993@gmail.com>
2022-12-30 16:17:20 +0200

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -14,7 +14,7 @@ if [[ $(hostname) == 'easdf' ]]; then
 elif [[ $(hostname) == 'server' ]]; then
   tmux send-key -t main:4 "cd ~/python; python3 -m aeacus.dummy_server --certificate ../resources/aeacus_secrets/fullchain.pem --private-key ../resources/aeacus_secrets/privkey.pem" Enter
 elif [[ $(hostname) == 'upf' ]]; then
-  tmux send-key -t main:2 "sudo ip route del default; sudo ip route add default via 10.0.10.14 dev br0" Enter
+  tmux send-key -t main:2 "sudo ip route del default; sudo ip route add default via 10.0.10.12 dev br0" Enter
   tmux send-key -t main:4 "cd ~/python; python3 -m aeacus.dummy_client https://aeacus.xuebing.me:4433" Enter
 elif [[ $(hostname) == 'asdf' ]]; then
   echo 'Nothing to do'
