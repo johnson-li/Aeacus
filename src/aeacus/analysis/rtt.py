4860b4cfa0b23bbff8dd968ad2dc5ceb637ce657
Johnson <johnsonli1993@gmail.com>
2023-07-31 11:59:21 +0300

    update rtt measurement

--- src/aeacus/analysis/rtt.py
+++ src/aeacus/analysis/rtt.py
@@ -6,8 +6,9 @@ import socket
 from aeacus import RESOURCE_PATH
 from icmplib import resolve, multiping
 from icmplib.models import Host
+from dnslib import DNSRecord, EDNS0, DNSLabel
 
-from aeacus.dns.resolver import resolve_name_iteratively_async
+from aeacus.dns.resolver import resolve_name_iteratively_async, resolve_ns_async
 
 
 async def resolve_domain_names(domain_names):
@@ -23,10 +24,25 @@ async def resolve_domain_names(domain_names):
     return res
 
 
+async def resolve_domain_names_ns(domain_names):
+    res = {}
+    for i, d in enumerate(domain_names):
+        try:
+            _, ns_name, _ = await resolve_ns_async(DNSLabel(d))
+            _, ns_ip, _ = await resolve_name_iteratively_async(DNSLabel(ns_name), resolver="127.0.0.53")
+            res[d] = {'name': str(ns_name), 'ip': str(ns_ip)}
+            print(f'[{i}] {d}: {res[d]}')
+        except Exception as e:
+            print(e)
+    json.dump(res, open(os.path.join(RESOURCE_PATH, 'alexa_top_500_ns_ip.json'), 'w+'))
+    return res
+
+
 def dump_resolved_ip():
     domains = open(os.path.join(RESOURCE_PATH, 'alexa_top_500.txt')).readlines()
     domains = [d.strip() for d in domains]
-    asyncio.run(resolve_domain_names(domains))
+    # asyncio.run(resolve_domain_names(domains))
+    asyncio.run(resolve_domain_names_ns(domains))
 
 
 def measure_rtt_to_server():
@@ -44,7 +60,17 @@ def measure_rtt_to_server():
 
 
 def measure_rtt_to_ns():
-    pass
+    data = json.load(open(os.path.join(RESOURCE_PATH, 'alexa_top_500_ns_ip.json')))
+    domains = list(data.keys())
+    ips = [data[d]['ip'] for d in domains]
+    res = multiping(ips, count=10, interval=0.1)
+    data = {}
+    for i, r in enumerate(res):
+        r: Host = r
+        assert ips[i] == r.address
+        if r.min_rtt > 0:
+            data[domains[i]] = r.min_rtt
+    json.dump(data, open(os.path.join(RESOURCE_PATH, 'alexa_top_500_ns_rtt.json'), 'w+'))
 
 
 def draw_diagram():
@@ -55,7 +81,8 @@ def draw_diagram():
 def main():
     # dump_resolved_ip()
     # measure_rtt_to_server()
-    draw_diagram()
+    measure_rtt_to_ns()
+    # draw_diagram()
 
 
 if __name__ == '__main__':
